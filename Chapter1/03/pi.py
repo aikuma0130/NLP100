ea628cdc7a42e5fe05913ecf5130164207b8dce7
aikuma <takahiro.kida.0130@gmail.com>
2016-10-14 22:35:08 +0900

    Add print

--- Chapter1/03/pi.py
+++ Chapter1/03/pi.py
@@ -1,2 +1,4 @@
 sentence = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
 words = [ len(w.rstrip(',.')) for w in sentence.split(' ')]
+
+print(words)
