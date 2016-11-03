60273ee4b43616195dc287d43216e6ea436e818b
aikuma <takahiro.kida.0130@gmail.com>
2016-11-03 15:58:49 +0900

    Resolve Chapter3 23

--- Chapter3/23/extract_chapter.py
+++ Chapter3/23/extract_chapter.py
@@ -1,17 +1,25 @@
 import re
 
-def extract_chapter(text):
-    chapter_pattern = re.compile('^==([^=]+)==$')
-    chapter_number_pattern = re.compile('^(=*)')
-    chapter_name_pattern = re.compile('^=*([^=][^=]*)=*$')
+def extract_chapter(text, level=1, number=[]):
+    chapter_pattern = re.compile('==(.*)==')
+    chapter_level_pattern = re.compile('^=(=*)')
+    chapter_name_pattern = re.compile('^=*([^=]+)=*$')
     result = []
+    pre = 0
+    chapter_index = [ 0 for i in range(5) ]
     for line in text.split('\n')[:-1]:
-        for chapter in chapter_pattern.findall(line):
-            number = len(chapter_number_pattern.search(chapter).group(1)) + 1
-            name = chapter_name_pattern.findall(chapter)[0]
-            result.append({ "name": name, "number": number })
-    #return result
-    return "\n".join([ str(data["number"]) + " " + data["name"] for data in result ])
+        if chapter_pattern.match(line):
+            name = chapter_name_pattern.findall(line)[0].strip()
+            level = len(chapter_level_pattern.search(line).group(1)) - 1
+
+            chapter_index[level] = chapter_index[level] + 1
+            for i, v in enumerate(chapter_index):
+                if i > level:
+                   chapter_index[i] = 0
+
+            result.append({ "name": name, "number": ".".join( str(index) for index in chapter_index[:level+1]) })
+
+    return '\n'.join([ "{0:<6}".format(data['number']) + " " + data['name'] for data in result ])
 
 if __name__ == '__main__':
     import sys
