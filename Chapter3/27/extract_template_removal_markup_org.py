5d23fe654b9b0a3f0e5dc20e0131c3d40b8fa70d
aikuma <takahiro.kida.0130@gmail.com>
2016-11-22 12:22:19 +0000

    Resolve Chapter3 27

---
+++ Chapter3/27/extract_template_removal_markup_org.py
@@ -0,0 +1,29 @@
+import re
+
+def extract_template(text):
+    basic_pattern = re.compile('{{基礎情報.*?\n\|(.*?)\n}}', re.DOTALL)
+    result = {}
+    for i in basic_pattern.findall(text):
+        for j in i.split('\n|'):
+            key = j.split(' = ')[0].lstrip()
+            value = j.split(' = ')[1].rstrip()
+            if key not in result:
+                result[key] = removal_markup(value)
+    return result
+
+def removal_markup(text):
+    max_emphasis_pattern = re.compile("'''''([^']+?)'''''", re.DOTALL)
+    mid_emphasis_pattern = re.compile("'''([^']+?)'''", re.DOTALL)
+    emphasis_pattern = re.compile("''([^']+?)''", re.DOTALL)
+    text = max_emphasis_pattern.sub('\g<1>', text)
+    text = mid_emphasis_pattern.sub('\g<1>', text)
+    text = emphasis_pattern.sub('\g<1>', text)
+    return text
+
+if __name__ == '__main__':
+    import sys
+    sys.path.append('../20/')
+    from extract_country import extract_country
+    
+    text = extract_country('../jawiki-country.json.gz', 'イギリス')
+    print(extract_template(text)['確立形態1'])
