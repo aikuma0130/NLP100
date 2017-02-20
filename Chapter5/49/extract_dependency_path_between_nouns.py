bf715b26521e1f46d2305876198bdab77e261803
Dakie <ktaka0130@gmail.com>
2017-02-21 02:45:00 +0900

    Solve Chapter5 49

--- Chapter5/49/extract_dependency_path_between_nouns.py
+++ Chapter5/49/extract_dependency_path_between_nouns.py
@@ -12,7 +12,8 @@ class Morph():
         self.pos1 = pos1
 
 class Chunk():
-    def __init__(self, morphs, dst, srcs):
+    def __init__(self, index, morphs, dst, srcs):
+        self.index = index
         self.morphs = morphs
         self.surfaces = "".join([ morph.surface for morph in morphs if morph.surface not in ['、','。']])
         self.dst = dst
@@ -58,7 +59,7 @@ class Chunk():
             root = ET.fromstring(sentence)
             chunks = []
             dependencies = {}
-            for chunk in root:
+            for index, chunk in enumerate(root):
                 src = int(chunk.attrib['id'])
                 dst = int(chunk.attrib['link'])
                 dependencies[src] = dst
@@ -70,26 +71,66 @@ class Chunk():
                     pos = tok.attrib['feature'].split(',')[0]
                     pos1 = tok.attrib['feature'].split(',')[1]
                     morphs.append(Morph(surface=surface, base=base, pos=pos, pos1=pos1))
-                chunks.append(Chunk(morphs, dst, srcs))
+                chunks.append(Chunk(index, morphs, dst, srcs))
             result.append(chunks)
         return result
 
     @staticmethod
-    def get_dst_surfaces(sentence, index):
-        surfaces = [ sentence[index].surfaces, ]
+    def get_chunks_until_root(sentence, index):
+        chunks = [ sentence[index], ]
         dst = sentence[index].dst
         if dst != -1:
-            surfaces.extend(Chunk.get_dst_surfaces(sentence, dst))
-        return surfaces
+            chunks.extend(Chunk.get_chunks_until_root(sentence, dst))
+        return chunks
 
     @staticmethod
     def extract_noun_path(sentence):
+        result = []
         for index, chunk in enumerate(sentence):
             if not chunk.has_pos('名詞'):
                 continue
-            surfaces = Chunk.get_dst_surfaces(sentence, index)
-            if len(surfaces) >= 2:
-                print(" -> ".join(surfaces))
+            chunks_until_root = Chunk.get_chunks_until_root(sentence, index)
+            if len(chunks_until_root) >= 2:
+                result.append(chunks_until_root)
+        return result
+
+    @staticmethod
+    def extract_noun_pair(sentence):
+        result = []
+        for index, chunk in enumerate(sentence):
+            if not chunk.has_pos('名詞'):
+                continue
+            for i in range(index+1, len(sentence)):
+                if sentence[i].has_pos('名詞'):
+                    result.append([chunk, sentence[i]])
+        return result
+
+    @staticmethod
+    def has_chunk(chunks, chunk):
+        for ch in chunks:
+            if ch.index == chunk.index:
+                return True
+        return False
+        
+    @staticmethod
+    def get_shared_chunk(chunks1, chunks2):
+        for ch1 in chunks1:
+            for ch2 in chunks2:
+                if ch1.index == ch2.index:
+                    return ch1
+        return None
+
+    @staticmethod
+    def mask_noun(chunk, mask):
+        surfaces = []
+        first = True
+        for morph in chunk.morphs:
+            if first and morph.pos == '名詞':
+                surfaces.append(mask)
+                first = False
+            else:
+                surfaces.append(morph.surface)
+        return "".join(surfaces)
 
 if __name__ == '__main__':
     
@@ -99,4 +140,44 @@ if __name__ == '__main__':
     #print([ chunk.surfaces for chunk in sentences[7]])
     #Chunk.extract_noun_path(sentences[7])
     for sentence in sentences:
-        Chunk.extract_noun_path(sentence)
+        noun_pairs = Chunk.extract_noun_pair(sentence)
+        for start_noun_chunk, end_noun_chunk in noun_pairs:
+            start_noun_path_to_root = Chunk.get_chunks_until_root(sentence, start_noun_chunk.index)
+            if Chunk.has_chunk(start_noun_path_to_root, end_noun_chunk):
+                result = []
+                for chunk in start_noun_path_to_root:
+                    if chunk.index == start_noun_chunk.index:
+                        result.append(Chunk.mask_noun(chunk,'X'))
+                    elif chunk.index == end_noun_chunk.index:
+                        result.append(Chunk.mask_noun(chunk,'Y'))
+                        break
+                    else:
+                        result.append(chunk.surfaces)
+                print(" -> ".join(result))
+            else:
+                end_noun_path_to_root = Chunk.get_chunks_until_root(sentence, end_noun_chunk.index)
+                shared_chunk = Chunk.get_shared_chunk(start_noun_path_to_root, end_noun_path_to_root)
+                if shared_chunk == None:
+                    print("None!!")
+                    sys.exit()
+                result = { "i": [],
+                           "j": []
+                         }
+                for chunk in start_noun_path_to_root:
+                    if chunk.index == shared_chunk.index:
+                        break
+                    if chunk.index == start_noun_chunk.index:
+                        result['i'].append(Chunk.mask_noun(chunk,'X'))
+                    else:
+                        result["i"].append(chunk.surfaces)
+                for chunk in end_noun_path_to_root:
+                    if chunk.index == shared_chunk.index:
+                        break
+                    if chunk.index == end_noun_chunk.index:
+                        result['j'].append(Chunk.mask_noun(chunk,'Y'))
+                    else:
+                        result["j"].append(chunk.surfaces)
+                print( " -> ".join(result["i"]) + " | " + 
+                       " -> ".join(result["j"]) + " | " +
+                       shared_chunk.surfaces
+                     )
