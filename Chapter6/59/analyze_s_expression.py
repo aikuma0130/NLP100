2747d31bed31b13e76ee544eb675a9299743df39
Dakie <ktaka0130@gmail.com>
2017-03-16 02:24:53 +0900

    Solve Chapter6 59

--- Chapter6/59/analyze_s_expression.py
+++ Chapter6/59/analyze_s_expression.py
@@ -7,7 +7,7 @@ import pydot
 
 class Sentence():
     def __init__(self, sentence):
-        self.lines = re.sub('([.;:?!]) ([A-Z])', "\g<1>\n\g<2>", sentence.replace('\n', '')).split('\n')
+        self.lines = re.sub('([.;:?!]) ([A-Z])', "\g<1>\n\g<2>", sentence.replace('\n', ' ')).split('\n')
         self.words = self._get_words()
 
     def _get_words(self):
@@ -18,33 +18,84 @@ class Sentence():
             words.append('')
         return words
 
+class SExpression():
+
+    @staticmethod
+    def parse_np(raw_s_expr, recursive=True):
+        match = re.search('\(NP ', raw_s_expr)
+        if not match:
+            return
+        parenthesis_count = 0
+        attribute = word = ''
+        words = []
+        attr = False
+        #print(raw_s_expr)
+        #print(match.start())
+        np_s_expr = raw_s_expr[match.start()+4:]
+        #print(np_s_expr)
+        for char_i, char in enumerate(np_s_expr):
+            if char == '(':
+                parenthesis_count += 1
+                attr = True
+                continue
+            elif char == ')':
+                parenthesis_count -= 1
+                if parenthesis_count == -1:
+                    index = char_i + 1
+                    break
+                else:
+                    words.append(word)
+                    attribute = word = ''
+                    continue
+            elif char == ' ':
+                attr = False
+                if attribute == 'NP':
+                    #print('attribute : ' + attribute)
+                    SExpression.parse_np(np_s_expr[char_i-3:], recursive=False)
+                continue
+            else:
+                if attr:
+                    attribute += char
+                else:
+                    word += char
+        print(" ".join(words))
+        if index != len(np_s_expr) and recursive:
+            SExpression.parse_np(np_s_expr[index:])
+
 if __name__ == '__main__':
     with open('../nlp.txt', 'r') as f:
         strings = f.read()
     sentence = Sentence(strings)
 
     parser = corenlp.StanfordCoreNLP(corenlp_path='/usr/local/lib/stanford-corenlp/', memory="3g")
-    parse_result = parser.raw_parse('Bills on ports and immigration were submitted by Senator Brownback, Republican of Kansas.')
-    print(json.dumps(parse_result, indent=4))
-    sys.exit()
-    #for line in sentence.lines:
-    #    parse_result = parser.raw_parse(line)
-    #    words = parse_result['sentences'][0]['words']
-    #    dependencies = parse_result['sentences'][0]['dependencies']
+    #parse_result = parser.raw_parse('Bills on ports and immigration were submitted by Senator Brownback, Republican of Kansas.')
+    #s_expression = SExpression.parse(parse_result['sentences'][0]['parsetree'])
+    for line in sentence.lines:
+        #print(line)
+        parse_result = parser.raw_parse(line)
+        words = parse_result['sentences'][0]['words']
+        dependencies = parse_result['sentences'][0]['dependencies']
+        print(parse_result['sentences'][0]['parsetree'])
+        print('')
+        SExpression.parse_np(parse_result['sentences'][0]['parsetree'])
+        #np_list = SExpression.parse_np(parse_result['sentences'][0]['parsetree'])
 
-    #    pred_candidate = {}
-    #    for word, info in words:
-    #        pred_candidate[word] = {
-    #                        "subj": "",
-    #                        "obj": ""
-    #                       }
+        #print(json.dumps(s_expression, indent=4))
+        sys.exit()
 
-    #    for dependency, src_word, dst_word in dependencies:
-    #        if re.match('nsubj', dependency):
-    #            pred_candidate[src_word]["subj"] = dst_word
-    #        elif re.match('dobj', dependency):
-    #            pred_candidate[src_word]["obj"] = dst_word
+        pred_candidate = {}
+        for word, info in words:
+            pred_candidate[word] = {
+                            "subj": "",
+                            "obj": ""
+                           }
 
-    #    for pred, part_of_speech in pred_candidate.items():
-    #        if part_of_speech["subj"] != "" and part_of_speech["obj"] != "":
-    #            print(part_of_speech["subj"] + "\t" + pred + "\t" + part_of_speech['obj'])
+        for dependency, src_word, dst_word in dependencies:
+            if re.match('nsubj', dependency):
+                pred_candidate[src_word]["subj"] = dst_word
+            elif re.match('dobj', dependency):
+                pred_candidate[src_word]["obj"] = dst_word
+
+        for pred, part_of_speech in pred_candidate.items():
+            if part_of_speech["subj"] != "" and part_of_speech["obj"] != "":
+                print(part_of_speech["subj"] + "\t" + pred + "\t" + part_of_speech['obj'])
