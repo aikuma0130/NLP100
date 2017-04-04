559d6b0b24f1109d5fe0dcc826030b9f401ebffe
Dakie <ktaka0130@gmail.com>
2017-04-05 00:21:44 +0900

    Solve Chapter7 64

--- Chapter7/64/construction_mongodb.py
+++ Chapter7/64/construction_mongodb.py
@@ -1,5 +1,6 @@
 import json
 import gzip
+import pprint
 import pymongo
 from pymongo import MongoClient
 
@@ -10,14 +11,17 @@ def main():
     with gzip.open('../artist.json.gz', 'r') as f:
         for line in f:
             info = json.loads(line.decode())
+            aliases_name = [ alias.get('name', None) for alias in ( info['aliases'] if 'aliases' in info else [] ) ]
+            tags_value = [ tag.get('value', None) for tag in ( info['tags'] if 'tags' in info else [] ) ]
+            raging_value = info['rating'].get('value', None) if 'rating' in info else None
             artist = {
                 'name': info.get('name', None),
-                'aliases_name': info.get('aliases.name', None),
-                'tags_value': info.get('tags.value', None),
-                'rating_value': info.get('rating.value', None)
+                'aliases_name': aliases_name,
+                'tags_value': tags_value,
+                'rating_value': raging_value,
             }
             collection.insert_one(artist)
-    print(collection.find_one())
+    pprint.pprint(collection.find_one())
 
 if __name__ == '__main__':
     main()
