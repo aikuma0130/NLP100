f4dab4d3d9e12563d2e11403ed4acd9d521b927d
Dakie <ktaka0130@gmail.com>
2017-04-16 00:23:11 +0900

    Solve Chapter8 70

--- Chapter8/70/format_data.py
+++ Chapter8/70/format_data.py
@@ -1,8 +1,17 @@
 import sys
+import random
 
 def main():
-    with open('../rt-polaritydata/rt-polarity.pos', 'r') as f:
-            positive_sentences = f.read()
+    sentiments = []
+    with open('../rt-polaritydata/rt-polarity-utf8.pos', 'r') as f:
+        for data in f:
+            sentiments.append('+1 ' + data)
+    with open('../rt-polaritydata/rt-polarity-utf8.neg', 'r') as f:
+        for data in f:
+            sentiments.append('-1 ' + data)
+    random.shuffle(sentiments)
+    with open('../sentiment.txt', 'w') as sentiment_txt:
+        sentiment_txt.writelines(sentiments)
 
 if __name__ == '__main__':
     main()
