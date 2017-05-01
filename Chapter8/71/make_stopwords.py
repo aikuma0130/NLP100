0a7829eb07c0e2a92a2f4559825732cf4efa7a58
Dakie <ktaka0130@gmail.com>
2017-05-02 00:13:31 +0900

    Modify StopWords Alg

--- Chapter8/71/make_stopwords.py
+++ Chapter8/71/make_stopwords.py
@@ -10,14 +10,20 @@ def main():
                 else:
                     word_count[word] = 1
     sorted_word_count = sorted(word_count.items(), key=lambda x: x[1], reverse=True)
-    #word_sum = sum([ word_count[1] for word_count in sorted_word_count ])
+
+    percent = 0.05
+    end = int(len(sorted_word_count) * percent)
+    word_sum = sum([ word_count[1] for word_count in sorted_word_count ])
     stop_words = []
+    sorted_word_count.reverse()
     for index, word_count in enumerate(sorted_word_count):
-        if index == 200:
-            break
+        #if index == 2000:
+        #    break
+        if word_count[1] == 1 or word_count[1] >= 500:
+            continue
         stop_words.append(word_count[0])
-        #percent = word_count[1] * 100 / word_sum
-        #print(word_count[0] + ": " + str(percent) + "%")
+        #per = word_count[1] * 100 / word_sum
+        #print(word_count[0] + ": " + str(per) + "%, " + str(word_count[1]))
 
     with open('../stopwords.txt', 'w') as f:
         for word in stop_words:
