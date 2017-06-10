9bb6d9fe33eb1af4584f29405b907e930317c470
Dakie <ktaka0130@gmail.com>
2017-06-10 22:51:38 +0900

    Modify stopwords

--- Chapter8/71/make_stopwords.py
+++ Chapter8/71/make_stopwords.py
@@ -11,17 +11,19 @@ def main():
                     word_count[word] = 1
     sorted_word_count = sorted(word_count.items(), key=lambda x: x[1], reverse=True)
 
-    percent = 0.05
-    all_words_count_sum = sum([ word_count[1] for word_count in sorted_word_count ])
-    lower_limit = int(all_words_count_sum * percent)
-    upper_limit = all_words_count_sum - lower_limit
+    #percent = 0.05
+    #all_words_count_sum = sum([ word_count[1] for word_count in sorted_word_count ])
+    #lower_limit = int(all_words_count_sum * percent)
+    #upper_limit = all_words_count_sum - lower_limit
 
     stop_words = []
     sorted_word_count.reverse()
+
     for index, word_count in enumerate(sorted_word_count):
         #if index == 2000:
         #    break
-        if word_count[1] <= lower_limit or word_count[1] >= upper_limit:
+        #if word_count[1] <= lower_limit or word_count[1] >= upper_limit:
+        if word_count[1] <= 1 or word_count[1] >= 1000:
             stop_words.append(word_count[0])
         else:
             continue
